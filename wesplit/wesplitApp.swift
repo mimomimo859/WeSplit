62727c83996b93aae721720c772aa16287313cb8
mimomimo859 <104977028+mimomimo859@users.noreply.github.com>
2022-12-01 21:08:40 +0900

    Initial Commit

---
+++ wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by 村田　美壮 on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
